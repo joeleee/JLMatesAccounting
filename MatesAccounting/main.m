2174b5b006b5d80e012319f0704d128610adcc1d
李卓成 <zhuocheng.li@renren-inc.com>
2013-11-12 17:17:13 +0800

    Initial Commit

---
+++ MatesAccounting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MatesAccounting
+//
+//  Created by Lee on 13-11-12.
+//  Copyright (c) 2013年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAAppDelegate class]));
+    }
+}
