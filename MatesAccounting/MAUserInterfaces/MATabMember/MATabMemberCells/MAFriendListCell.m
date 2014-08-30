a96c250abdbf50445b84fbf205375a7f84dbd34c
Joe Lee <zhuocheng.lee@gmail.com>
2014-08-30 17:47:58 +0800

    Optimizing UI of friend list page
    Change-Id: Id3acc01e37e253106b6a049ab658c86c00c5ce71

--- MatesAccounting/MAUserInterfaces/MATabMember/MATabMemberCells/MAFriendListCell.m
+++ MatesAccounting/MAUserInterfaces/MATabMember/MATabMemberCells/MAFriendListCell.m
@@ -10,7 +10,7 @@
 
 #import "MFriend.h"
 
-@interface MAFriendListCell ()
+@interface MAFriendListCell () <MAManualLayoutAfterLayoutSubviewsProtocol>
 
 @property (weak, nonatomic) IBOutlet UILabel *friendNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *phoneTitleLabel;
@@ -20,6 +20,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *accountCountLabel;
 @property (weak, nonatomic) IBOutlet UILabel *groupTitleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *groupCountLabel;
+@property (weak, nonatomic) IBOutlet UIView *dividingLineView;
 
 @end
 
@@ -28,6 +29,7 @@
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
     if (self = [super initWithCoder:aDecoder]) {
+        [self needManualLayoutAfterLayoutSubviews];
     }
 
     return self;
@@ -40,10 +42,24 @@
     if (0 < data.eMail.length) {
         [self.eMailLabel setText:data.eMail];
     } else {
-        [self.eMailLabel setText:@"没有填写e-mail"];
+        [self.eMailLabel setText:@"With out E-mail"];
     }
     [self.accountCountLabel setText:[@(data.relationshipToAccount.count) stringValue]];
     [self.groupCountLabel setText:[@(data.relationshipToGroup.count) stringValue]];
+}
+
+#pragma mark MAManualLayoutAfterLayoutSubviewsProtocol
+- (void)manualLayoutAfterLayoutSubviews
+{
+    self.friendNameLabel.textColor = MA_COLOR_TABMEMBER_PAYER_NAME;
+    self.phoneTitleLabel.textColor = MA_COLOR_TABMEMBER_BALANCE_TITLE;
+    self.phoneNumberLabel.textColor = MA_COLOR_TABMEMBER_BALANCE_TITLE;
+    self.eMailLabel.textColor = MA_COLOR_TABMEMBER_BALANCE_TITLE;
+    self.accountTitleLabel.textColor = MA_COLOR_TABMEMBER_BALANCE_TITLE;
+    self.accountCountLabel.textColor = MA_COLOR_TABMEMBER_BALANCE;
+    self.groupTitleLabel.textColor = MA_COLOR_TABMEMBER_BALANCE_TITLE;
+    self.groupCountLabel.textColor = MA_COLOR_TABMEMBER_BALANCE;
+    self.dividingLineView.backgroundColor = MA_COLOR_TABMEMBER_DIVIDING_LINE;
 }
 
 + (CGFloat)cellHeight:(id)data
